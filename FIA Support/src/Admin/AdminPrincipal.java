317304e873c69ac1206e94b259ef9e6c052e02f2
cliente <cliente@DESKTOP-FO2EAEU>
2025-10-02 18:12:06 -0600

    Agregue una imagen

--- FIA Support/src/Admin/AdminPrincipal.java
+++ FIA Support/src/Admin/AdminPrincipal.java
@@ -25,6 +25,7 @@ public class AdminPrincipal extends javax.swing.JFrame
         btnGestionarTicket = new javax.swing.JButton();
         btnGestionarUsuario = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        lblLogo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,42 +41,47 @@ public class AdminPrincipal extends javax.swing.JFrame
         jToolBar1.add(lblFiaSupport);
 
         desktopPane.add(jToolBar1);
-        jToolBar1.setBounds(0, 0, 360, 70);
+        jToolBar1.setBounds(0, 0, 270, 110);
 
         btnReportesYEstadisticas.setBackground(new java.awt.Color(102, 102, 102));
         btnReportesYEstadisticas.setText("Reportes y estadisticas ");
         desktopPane.add(btnReportesYEstadisticas);
-        btnReportesYEstadisticas.setBounds(90, 100, 160, 25);
+        btnReportesYEstadisticas.setBounds(90, 140, 160, 25);
 
         btnAsignarSolicitud.setBackground(new java.awt.Color(102, 102, 102));
         btnAsignarSolicitud.setText("Asignar solicitud");
         desktopPane.add(btnAsignarSolicitud);
-        btnAsignarSolicitud.setBounds(90, 170, 160, 25);
+        btnAsignarSolicitud.setBounds(90, 210, 160, 25);
 
         btnGestionarTicket.setBackground(new java.awt.Color(102, 102, 102));
         btnGestionarTicket.setText("Gestionar ticket");
         desktopPane.add(btnGestionarTicket);
-        btnGestionarTicket.setBounds(90, 240, 160, 25);
+        btnGestionarTicket.setBounds(90, 280, 160, 25);
 
         btnGestionarUsuario.setBackground(new java.awt.Color(102, 102, 102));
         btnGestionarUsuario.setText("Gestionar usuario");
         desktopPane.add(btnGestionarUsuario);
-        btnGestionarUsuario.setBounds(90, 310, 160, 25);
+        btnGestionarUsuario.setBounds(90, 350, 160, 25);
 
         jButton1.setBackground(new java.awt.Color(102, 102, 102));
         jButton1.setText("Cerrar sesión");
         desktopPane.add(jButton1);
-        jButton1.setBounds(90, 380, 160, 25);
+        jButton1.setBounds(90, 420, 160, 25);
+
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Fia_Support_Logo-convertido-a-100x100 (2).png"))); // NOI18N
+        lblLogo.setText("jLabel1");
+        desktopPane.add(lblLogo);
+        lblLogo.setBounds(270, 0, 100, 110);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 361, Short.MAX_VALUE)
+            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(desktopPane, javax.swing.GroupLayout.PREFERRED_SIZE, 483, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(desktopPane, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 483, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -115,6 +121,7 @@ public class AdminPrincipal extends javax.swing.JFrame
     private javax.swing.JButton jButton1;
     private javax.swing.JToolBar jToolBar1;
     private javax.swing.JLabel lblFiaSupport;
+    private javax.swing.JLabel lblLogo;
     // End of variables declaration//GEN-END:variables
 
 }
